e0ecea552852349d15761b2da430d2e3ea7931f5
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-05-30 16:03:14 +0200

    lll

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.luca020400.zxing"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
